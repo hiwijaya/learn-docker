b92cfc20754852af77d9c8d983afe44f38446de6
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-09-14 20:13:24 +0700

    set environment variables

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import os
+# from dotenv import load_dotenv
+
 
 app = Flask(__name__)
+# load_dotenv()
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello ' + os.environ.get('NAME')
 
 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
